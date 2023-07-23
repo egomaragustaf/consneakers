b4c341bc2a7609e7a33093044aa457d9590886ae
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-23 21:09:10 +0700

    feat: display products

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import { json, type V2_MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -7,10 +8,27 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+export async function loader() {
+  const products = [
+    {
+      id: 1,
+      name: "Converse Run Star Hike Hi Black White",
+      price: 1_299_000,
+      createdAt: new Date(),
+    },
+  ];
+
+  return json({ products });
+}
+
 export default function Index() {
+  const { products } = useLoaderData<typeof loader>();
+
   return (
     <main>
       <h1>Consneakers</h1>
+
+      <pre>{JSON.stringify(products, null, 2)}</pre>
     </main>
   );
 }
