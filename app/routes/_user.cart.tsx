ad187ca4ec726983eb891e94b1a8facd2dd34c4d
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-30 22:10:03 +0700

    feat: cart route display cart data

---
+++ app/routes/_user.cart.tsx
@@ -0,0 +1,36 @@
+import { json, type LoaderArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Layout } from "~/components";
+import { prisma } from "~/db.server";
+import { authenticator } from "~/services";
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const userSession = await authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+  });
+
+  const existingCart = await prisma.cart.findFirst({
+    where: { userId: userSession.id },
+    include: { cartItems: true },
+  });
+
+  if (!existingCart) {
+    const newCart = await prisma.cart.create({
+      data: { userId: userSession.id },
+      include: { cartItems: true },
+    });
+    return json({ cart: newCart });
+  }
+
+  return json({ cart: existingCart });
+};
+
+export default function RouteComponent() {
+  const { cart } = useLoaderData<typeof loader>();
+
+  return (
+    <Layout>
+      <pre>{JSON.stringify(cart, null, 2)}</pre>
+    </Layout>
+  );
+}
