2154ba506e03c4ce91da7d43ced5da3fb564f474
Ego Maragustaf <egomaragustaf@gmail.com>
2023-07-31 21:40:40 +0700

    feat: implement edit data and update with slug

--- app/routes/admin.products.edit.$slug.tsx
+++ app/routes/admin.products.edit.$slug.tsx
@@ -8,7 +8,7 @@ import { zfd } from "zod-form-data";
 
 import { prisma } from "~/db.server";
 import { Layout } from "~/components/layout/layout";
-import { slugify } from "~/utils/slugify";
+import { slugify } from "~/utils";
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -19,24 +19,28 @@ export const meta: V2_MetaFunction = () => {
 
 const schema = zfd.formData({
   name: zfd.text(),
+  slug: zfd.text(),
   price: zfd.numeric(z.number().min(0).max(100_000_000)),
   description: zfd.text(),
 });
 
 export async function loader({ params }: LoaderArgs) {
-  const slug = params.slug;
-  const products = await prisma.product.findMany({
+  if (!params.slug) {
+    return json({ product: null });
+  }
+
+  const product = await prisma.product.findUnique({
     where: {
-      slug: slug,
+      slug: params.slug,
     },
   });
 
-  return json({ products });
+  return json({ product });
 }
 
 export default function EditProductRoute() {
   const lastSubmission = useActionData<typeof action>();
-  const { products } = useLoaderData<typeof loader>();
+  const { product } = useLoaderData<typeof loader>();
   const [form, fields] = useForm({
     lastSubmission,
     onValidate({ formData }) {
@@ -44,7 +48,7 @@ export default function EditProductRoute() {
     },
   });
 
-  if (!products) {
+  if (!product) {
     return <p>Sorry, no product found.</p>;
   }
 
@@ -56,56 +60,59 @@ export default function EditProductRoute() {
           <p>add product below</p>
         </header>
 
-        {products.map((product) => {
-          return (
-            <Form
-              key={product.id}
-              method="POST"
-              {...form.props}
-              className="text-slate-700 w-full max-w-xl text-lg rounded-lg border bg-white p-4">
-              <div>
-                <label htmlFor="name" className="mb-2">
-                  Product name:
-                </label>
-                <input
-                  {...conform.input(fields.name)}
-                  id="name"
-                  className="w-full px-2 py-1 rounded-md border-gray-300 border"
-                  defaultValue={product.name}
-                />
-                <p>{fields.name.error}</p>
-              </div>
+        <Form
+          key={product.id}
+          method="POST"
+          {...form.props}
+          className="text-slate-700 w-full max-w-xl text-lg rounded-lg border bg-white p-4">
+          <input
+            {...conform.input(fields.slug)}
+            type="hidden"
+            name="slug"
+            defaultValue={product.slug}
+          />
 
-              <div>
-                <label htmlFor="price">Product price:</label>
-                <input
-                  {...conform.input(fields.price, { type: "number" })}
-                  id="price"
-                  className="w-full px-2 py-1 rounded-md border-gray-300 border"
-                  defaultValue={product.price}
-                />
-                <p>{fields.price.error}</p>
-              </div>
+          <div>
+            <label htmlFor="name" className="mb-2">
+              Product name:
+            </label>
+            <input
+              {...conform.input(fields.name)}
+              id="name"
+              className="w-full px-2 py-1 rounded-md border-gray-300 border"
+              defaultValue={product.name}
+            />
+            <p>{fields.name.error}</p>
+          </div>
 
-              <div>
-                <label htmlFor="description">Product description:</label>
-                <input
-                  {...conform.input(fields.description)}
-                  id="description"
-                  className="w-full px-2 py-1 rounded-md border-gray-300 border"
-                  defaultValue={product.description || ""}
-                />
-                <p>{fields.description.error}</p>
-              </div>
+          <div>
+            <label htmlFor="price">Product price:</label>
+            <input
+              {...conform.input(fields.price, { type: "number" })}
+              id="price"
+              className="w-full px-2 py-1 rounded-md border-gray-300 border"
+              defaultValue={product.price}
+            />
+            <p>{fields.price.error}</p>
+          </div>
 
-              <button
-                type="submit"
-                className="w-full py-2 rounded-md border-gray-300 text-white bg-slate-800 hover:bg-slate-900 border mt-4">
-                Update Product
-              </button>
-            </Form>
-          );
-        })}
+          <div>
+            <label htmlFor="description">Product description:</label>
+            <input
+              {...conform.input(fields.description)}
+              id="description"
+              className="w-full px-2 py-1 rounded-md border-gray-300 border"
+              defaultValue={product.description || ""}
+            />
+            <p>{fields.description.error}</p>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full py-2 rounded-md border-gray-300 text-white bg-slate-800 hover:bg-slate-900 border mt-4">
+            Update Product
+          </button>
+        </Form>
       </main>
     </Layout>
   );
@@ -119,15 +126,15 @@ export async function action({ request }: ActionArgs) {
     return json(submission, { status: 400 });
   }
 
-  const slug = slugify(submission.value.name);
+  const slug = submission.value.slug;
 
-  const newProduct = await prisma.product.create({
-    data: { ...submission.value, slug },
+  const updateProduct = await prisma.product.update({
+    where: { slug: slug },
+    data: { ...submission.value, slug: slugify(submission.value.name) },
   });
 
-  if (!newProduct) {
+  if (!updateProduct) {
     return json(submission, { status: 500 });
   }
-
-  return redirect(`/admin/products`);
+  return redirect("/admin/products");
 }
