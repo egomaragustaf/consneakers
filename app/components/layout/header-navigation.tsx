7f3c6f9b5804599b00dab41a17415091e7836934
Ego Maragustaf <egomaragustaf@gmail.com>
2023-08-01 19:37:28 +0700

    feat: split search-form reusable compononent

--- app/components/layout/header-navigation.tsx
+++ app/components/layout/header-navigation.tsx
@@ -1,17 +1,19 @@
 import { Link } from "@remix-run/react";
-import { Input } from "../ui";
+import { SearchForm } from "~/components";
 
 export function Navigation() {
   return (
     <header className="z-10 flex w-full items-center justify-between gap-6 px-20 bg-zinc-800 text-white">
       <div>
-        <span>CONSNEAKERS</span>
+        <Link to={`/`}>
+          <span>CONSNEAKERS</span>
+        </Link>
       </div>
 
-      <div className="w-full py-4">
-        <Input className="max-w-2xl" />
+      <div className="w-full py-4 flex flex-col gap-2">
+        <SearchForm />
         <nav className="w-full max-w-md text-sm flex justify-start items-center">
-          <ul className="flex w-full gap-8 p-2">
+          <ul className="flex w-full gap-8">
             <li>
               <Link to={`/`}>Home</Link>
             </li>
@@ -20,6 +22,12 @@ export function Navigation() {
             </li>
           </ul>
         </nav>
+      </div>
+
+      <div>
+        <Link to={`/dashboard`}>
+          <span>Login</span>
+        </Link>
       </div>
     </header>
   );
---
+++ app/components/shared/search-form.tsx
@@ -0,0 +1,33 @@
+import { useSearchParams } from "@remix-run/react";
+import { Input } from "~/components";
+
+interface Props {
+  action?: string;
+  placeholder?: string;
+  className?: string;
+}
+
+export function SearchForm({
+  action = "/search",
+  placeholder = "Search...",
+}: Props) {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("q") || "";
+
+  return (
+    <form method="GET" action={action} className="w-full">
+      <fieldset className="relative flex items-center gap-1">
+        <label htmlFor="search" />
+        <Input
+          id="search"
+          type="search"
+          name="q"
+          placeholder={placeholder}
+          defaultValue={query}
+          autoComplete="off"
+          className="max-w-2xl bg-zinc-700 focus:bg-zinc-50 transition duration-100 text-black"
+        />
+      </fieldset>
+    </form>
+  );
+}
