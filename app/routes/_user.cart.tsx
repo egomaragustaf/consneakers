0eb5cdeb4a7489bb2c25426ee952a5a0a6e75c52
Ego Maragustaf <egomaragustaf@gmail.com>
2023-08-04 02:24:52 +0700

    feat: set user product cart and implement add to cart

--- app/routes/_user.cart.tsx
+++ app/routes/_user.cart.tsx
@@ -1,10 +1,27 @@
-import { json, type LoaderArgs } from "@remix-run/node";
+import { json, type LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { Layout } from "~/components";
 import { prisma } from "~/db.server";
 import { authenticator } from "~/services";
 
-export const loader = async ({ request }: LoaderArgs) => {
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+}
+
+interface CartItem {
+  id: string;
+  quantity: number;
+  product: Product;
+}
+
+interface Cart {
+  cartItems: CartItem[];
+  totalPrice: number;
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
   const userSession = await authenticator.isAuthenticated(request, {
     failureRedirect: "/login",
   });
@@ -22,15 +39,49 @@ export const loader = async ({ request }: LoaderArgs) => {
     return json({ cart: newCart });
   }
 
-  return json({ cart: existingCart });
+  const cartItemsMap = new Map<string, CartItem>();
+  for (const cartItem of existingCart.cartItems) {
+    if (cartItemsMap.has(cartItem.product.id)) {
+      cartItemsMap.get(cartItem.product.id)!.quantity += cartItem.quantity;
+    } else {
+      cartItemsMap.set(cartItem.product.id, { ...cartItem });
+    }
+  }
+
+  const aggregatedCartItems = Array.from(cartItemsMap.values());
+
+  const totalPrice = aggregatedCartItems.reduce(
+    (total, cartItem) => total + cartItem.product.price * cartItem.quantity,
+    0
+  );
+
+  return json({ cart: { cartItems: aggregatedCartItems, totalPrice } });
 };
 
-export default function RouteComponent() {
-  const { cart } = useLoaderData<typeof loader>();
+export default function Route() {
+  const { cart } = useLoaderData<{ cart: Cart }>();
 
   return (
     <Layout>
-      <pre>{JSON.stringify(cart, null, 2)}</pre>
+      <main className="w-full max-w-7xl flex gap-8 justify-center items-start">
+        <article className="flex gap-8 w-full max-w-4xl">
+          <div className="flex flex-col flex-grow gap-4 w-1/3">
+            <header className="text-2xl font-bold">
+              <section className="flex flex-col gap-2"></section>
+            </header>
+            <section className="flex flex-col gap-2">
+              {cart.cartItems.map((cartItem) => (
+                <ul key={cartItem.id}>
+                  <li>Product name: {cartItem.product.name}</li>
+                  <li>Quantity: {cartItem.quantity}</li>
+                  <li>Price: {cartItem.product.price * cartItem.quantity}</li>
+                  <li>Total price: {cart.totalPrice}</li>
+                </ul>
+              ))}
+            </section>
+          </div>
+        </article>
+      </main>
     </Layout>
   );
 }
