4039b6d4ac57fcd3aacdda6602ada540642a0a17
Ego Maragustaf <egomaragustaf@gmail.com>
2023-08-08 01:42:45 +0700

    bug: fix redirect to login page if user logout

--- app/routes/_auth.logout.tsx
+++ app/routes/_auth.logout.tsx
@@ -2,9 +2,9 @@ import type { LoaderArgs, ActionArgs } from "@remix-run/node";
 import { authenticator } from "~/services/auth.server";
 
 export const loader = async ({ request }: LoaderArgs) => {
-  await authenticator.logout(request, { redirectTo: "/" });
+  await authenticator.logout(request, { redirectTo: "/login" });
 };
 
 export const action = async ({ request }: ActionArgs) => {
-  await authenticator.logout(request, { redirectTo: "/" });
+  await authenticator.logout(request, { redirectTo: "/login" });
 };
