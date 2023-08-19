49f83ce0a89de65e2541103d244c643e2aef723c
Ego Maragustaf <egomaragustaf@gmail.com>
2023-08-19 09:15:17 +0700

    feat: refine dashboard add new product

--- app/routes/admin.dashboard.new.tsx
+++ app/routes/admin.dashboard.new.tsx
@@ -42,66 +42,68 @@ export default function FormRoute() {
           <Sidebar />
         </div>
 
-        <div className="w-full gap-6 flex flex-col justify-center items-center">
-          <header className="space-y-2 w-full flex justify-start items-center">
+        <div className="w-full gap-6 flex flex-col justify-start items-center">
+          <header className="space-y-2 w-full">
             <h1 className="text-2xl">Add New Product</h1>
           </header>
 
-          <Form
-            method="POST"
-            {...form.props}
-            className="text-slate-700 w-full text-lg rounded-lg border bg-white p-4">
-            <div>
-              <label htmlFor="name" className="mb-2">
-                Product name:
-              </label>
-              <input
-                {...conform.input(fields.name)}
-                id="name"
-                className="w-full px-2 py-1 rounded-md border-gray-300 border"
-              />
-              <p>{fields.name.error}</p>
-            </div>
+          <section className="w-full flex justify-start items-center">
+            <Form
+              method="POST"
+              {...form.props}
+              className="text-slate-700 w-full max-w-xl text-lg rounded-lg border bg-white p-4">
+              <div>
+                <label htmlFor="name" className="mb-2">
+                  Product name:
+                </label>
+                <input
+                  {...conform.input(fields.name)}
+                  id="name"
+                  className="w-full px-2 py-1 rounded-md border-gray-300 border"
+                />
+                <p>{fields.name.error}</p>
+              </div>
 
-            <div>
-              <label htmlFor="price">Product price:</label>
-              <input
-                {...conform.input(fields.price, { type: "number" })}
-                id="price"
-                className="w-full px-2 py-1 rounded-md border-gray-300 border"
-              />
-              <p>{fields.price.error}</p>
-            </div>
+              <div>
+                <label htmlFor="price">Product price:</label>
+                <input
+                  {...conform.input(fields.price, { type: "number" })}
+                  id="price"
+                  className="w-full px-2 py-1 rounded-md border-gray-300 border"
+                />
+                <p>{fields.price.error}</p>
+              </div>
 
-            <div>
-              <label htmlFor="description">Product description:</label>
-              <input
-                {...conform.input(fields.description)}
-                id="description"
-                className="w-full px-2 py-1 rounded-md border-gray-300 border"
-              />
-              <p>{fields.description.error}</p>
-            </div>
+              <div>
+                <label htmlFor="description">Product description:</label>
+                <input
+                  {...conform.input(fields.description)}
+                  id="description"
+                  className="w-full px-2 py-1 rounded-md border-gray-300 border"
+                />
+                <p>{fields.description.error}</p>
+              </div>
 
-            <div>
-              <label htmlFor="imageURL">Add Image:</label>
-              <input
-                {...conform.input(fields.imageURL)}
-                id="imageURL"
-                className="w-full px-2 py-1 rounded-md border-gray-300 border"
-                accept="image/png, image/jpeg"
-              />
-              <p>{fields.description.error}</p>
-            </div>
+              <div>
+                <label htmlFor="imageURL">Add Image:</label>
+                <input
+                  {...conform.input(fields.imageURL)}
+                  id="imageURL"
+                  className="w-full px-2 py-1 rounded-md border-gray-300 border"
+                  accept="image/png, image/jpeg"
+                />
+                <p>{fields.description.error}</p>
+              </div>
 
-            <ButtonLoading
-              type="submit"
-              isSubmitting={isSubmitting}
-              submittingText="Adding..."
-              className="w-96 mt-4">
-              Add Product
-            </ButtonLoading>
-          </Form>
+              <ButtonLoading
+                type="submit"
+                isSubmitting={isSubmitting}
+                submittingText="Adding..."
+                className="w-96 mt-4">
+                Add Product
+              </ButtonLoading>
+            </Form>
+          </section>
         </div>
       </main>
     </Layout>
