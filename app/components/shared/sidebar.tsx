1e46eee1432e79febdd24d139208a1e5c55f2b2d
Ego Maragustaf <egomaragustaf@gmail.com>
2023-08-31 22:47:24 +0700

    feat: fix typo dashboard and refine side bar

--- app/components/shared/sidebar.tsx
+++ app/components/shared/sidebar.tsx
@@ -1,45 +1,57 @@
 import { cn } from "~/utils";
-import { Button, Separator } from "~/components";
-import { Link } from "@remix-run/react";
+import { buttonVariants } from "~/components";
+import { NavLink } from "@remix-run/react";
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
 }
 
+const adminSideBarItems = [
+  {
+    to: "/admin/dashboard/products",
+    text: "All Products",
+  },
+  {
+    to: "/admin/dashboard/new",
+    text: "Add New Product",
+  },
+  {
+    to: "/admin/dashboard/edit",
+    text: "Edit Product",
+  },
+  {
+    to: "/logout",
+    text: "Logout",
+  },
+];
+
 export function Sidebar({ className }: SidebarProps) {
   return (
-    <div className={cn("pb-4 bg-zinc-800 text-white rounded fixed", className)}>
+    <div className={cn("pb-4 rounded fixed", className)}>
       <div className="space-y-4 py-4">
         <div className="px-3 py-2">
           <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-            Dahsboard
+            Dashboard
           </h2>
           <div className="space-y-1 flex flex-col">
-            <Link to={`/admin/dashboard/products`}>
-              <Button variant="ghost" className="w-full justify-start">
-                All Products
-              </Button>
-            </Link>
-
-            <Link to={`/admin/dashboard/new`}>
-              <Button variant="ghost" className="w-full justify-start">
-                Add New Product
-              </Button>
-            </Link>
-
-            <Link to={`/admin/dashboard/edit`}>
-              <Button variant="ghost" className="w-full justify-start">
-                Edit Product
-              </Button>
-            </Link>
-
-            <Separator />
-
-            <Link to={`/logout`}>
-              <Button variant="ghost" className="w-full justify-start">
-                Logout
-              </Button>
-            </Link>
+            {adminSideBarItems.map((item) => {
+              return (
+                <NavLink
+                  key={item.to}
+                  to={item.to}
+                  className={({ isActive }) =>
+                    cn(
+                      buttonVariants({ variant: "ghost" }),
+                      isActive
+                        ? "bg-zinc-800 hover:bg-zinc-700 text-white hover:text-white"
+                        : "hover:bg-muted",
+                      "justify-start"
+                    )
+                  }>
+                  {item.text}
+                </NavLink>
+              );
+            })}
           </div>
         </div>
       </div>
