fa817371b9765b4852e578b0fd1e56afbd092c7b
Ego Maragustaf <egomaragustaf@gmail.com>
2023-09-06 15:28:37 +0700

    feat: add metadata checkout page

--- app/routes/_user.checkout.tsx
+++ app/routes/_user.checkout.tsx
@@ -1,4 +1,6 @@
-import { json, type LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import type { V2_MetaFunction, LoaderArgs } from "@remix-run/node";
+
 import { useLoaderData } from "@remix-run/react";
 
 import { Layout, Table, TableBody, TableCell, TableRow } from "~/components";
@@ -6,6 +8,10 @@ import { prisma } from "~/db.server";
 import type { getShoppingCart } from "~/models/cart.server";
 import { authenticator } from "~/services";
 import { formatValueToCurrency } from "~/utils";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "Checkout" }, { name: "description", content: "Checkout" }];
+};
 
 type LoaderData = {
   cart: Awaited<ReturnType<typeof getShoppingCart>>;
@@ -37,7 +43,15 @@ export default function Route() {
     <Layout>
       <main className="w-full max-w-7xl flex gap-8 justify-center items-start">
         <article className="flex lg:flex-row flex-col gap-16 w-full max-w-5xl min-h-screen">
-          <section className="flex flex-col gap-4 lg:w-2/3 max-w-3xl"></section>
+          <section className="flex flex-col gap-4 lg:w-2/3 max-w-3xl">
+            <header className="text-2xl font-bold">
+              <h1>Checkout</h1>
+            </header>
+
+            <main>
+              <h2>Delivery Address</h2>
+            </main>
+          </section>
 
           <section className="flex flex-col gap-4 lg:w-1/3 max-w-3xl">
             <header className="text-2xl font-bold">
