9f61651b555a2474b1670545dde40adc7c686831
Ego Maragustaf <egomaragustaf@gmail.com>
2023-09-06 15:13:47 +0700

    feat: render summary checkout with table

--- app/routes/_user.checkout.tsx
+++ app/routes/_user.checkout.tsx
@@ -1,29 +1,15 @@
 import { json, type LoaderArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { Layout } from "~/components";
+
+import { Layout, Table, TableBody, TableCell, TableRow } from "~/components";
 import { prisma } from "~/db.server";
+import type { getShoppingCart } from "~/models/cart.server";
 import { authenticator } from "~/services";
 import { formatValueToCurrency } from "~/utils";
 
-interface Product {
-  id: string;
-  name: string;
-  price: number;
-  description: string | null;
-  imageURL: string | null;
-}
-
-interface CartItem {
-  id: string;
-  quantity: number;
-  product: Product;
-  totalPrice: number;
-}
-
-interface Cart {
-  cartItems: CartItem[];
-  totalPrice: number;
-}
+type LoaderData = {
+  cart: Awaited<ReturnType<typeof getShoppingCart>>;
+};
 
 export const loader = async ({ request }: LoaderArgs) => {
   const userSession = await authenticator.isAuthenticated(request, {
@@ -49,7 +35,7 @@ export const loader = async ({ request }: LoaderArgs) => {
 };
 
 export default function Route() {
-  const { cart } = useLoaderData<{ cart: Cart }>();
+  const { cart } = useLoaderData<LoaderData>();
   const totalItemCount =
     cart?.cartItems.reduce((acc, item) => acc + item.quantity, 0) ?? 0;
   const grandTotal =
@@ -59,13 +45,25 @@ export default function Route() {
     <Layout>
       <main className="w-full max-w-7xl flex gap-8 justify-center items-start">
         <article className="flex lg:flex-row flex-col gap-16 w-full max-w-5xl min-h-screen">
-          <section className="flex flex-col gap-4 lg:w-2/3 max-w-3xl">
-            <header className="text-2xl font-bold">
-              <h1>My Cart</h1>
+          <section className="flex flex-col gap-4 lg:w-2/3 max-w-3xl"></section>
 
-              <h1>{formatValueToCurrency(grandTotal)}</h1>
-              <p>{totalItemCount}</p>
+          <section className="flex flex-col gap-4 lg:w-1/3 max-w-3xl">
+            <header className="text-2xl font-bold">
+              <h1>Summary</h1>
             </header>
+
+            <Table className="bg-zinc-100 rounded">
+              <TableBody>
+                <TableRow>
+                  <TableCell>Total Product:</TableCell>
+                  <TableCell>{totalItemCount}</TableCell>
+                </TableRow>
+                <TableRow className="text-lg font-bold text-zinc-800">
+                  <TableCell>Total Price:</TableCell>
+                  <TableCell>{formatValueToCurrency(grandTotal)}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
           </section>
         </article>
       </main>
