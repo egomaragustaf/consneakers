888f02427bc816881d769e10d4b3eb43ef5176bb
Ego Maragustaf <egomaragustaf@gmail.com>
2023-09-19 22:43:35 +0700

    style: customize increment decrement delete button color

--- app/components/ui/cart-item.tsx
+++ app/components/ui/cart-item.tsx
@@ -1,8 +1,8 @@
 import { Link, useFetcher } from "@remix-run/react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { MdOutlineDelete } from "react-icons/md";
-import { cn, formatValueToCurrency } from "~/utils";
-import { Separator } from "~/components";
+import { formatValueToCurrency } from "~/utils";
+import { Button, Input, Separator } from "~/components";
 
 function calculateCartItem({
   product,
@@ -85,33 +85,16 @@ export function CartItem({
         {/* CART ITEM COUNTER */}
         <div>
           <div className="mx-6 flex items-center gap-2">
-            {/* DELETE */}
-            <cartItemFetcher.Form method="post" action="/cart" className="mr-8">
-              <button
-                type="submit"
-                data-action-id="remove-from-cart"
-                className="block">
-                <span className="sr-only">Hapus dari keranjang</span>
-                <MdOutlineDelete className="h-6 w-6 text-gray-500 hover:text-red-800" />
-              </button>
-              <input type="hidden" name="_action" value="delete-item-in-cart" />
-              <input type="hidden" name="cartItemId" value={cartItem?.id} />
-              <input type="hidden" name="orderItemId" value={cartItem?.id} />
-            </cartItemFetcher.Form>
-            {/* DELETE */}
-
             {/* DECREMENT */}
             <cartItemFetcher.Form method="post" action="/cart">
-              <button
+              <Button
                 data-action-id="decrement-cart"
                 type="submit"
                 disabled={isLessThanMin}
-                className={cn(
-                  "text-green-500 hover:text-green-800",
-                  isLessThanMin && "text-gray-300 hover:text-gray-300"
-                )}>
-                <AiOutlineMinus className="h-6 w-6" />
-              </button>
+                variant={isLessThanMin ? "disabled" : "outline"}
+                className="border-zinc-400">
+                <AiOutlineMinus className="text-sm" />
+              </Button>
               <input
                 type="hidden"
                 name="_action"
@@ -134,28 +117,24 @@ export function CartItem({
 
             {/* QUANTITY */}
             <div>
-              <input
-                className="h-8 w-14 rounded border-gray-200 bg-gray-50 p-0 text-center text-gray-600 [-moz-appearance:_textfield] focus-visible:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
-                type="number"
+              <Input
                 disabled
+                className="text-center w-12 bg-slate-100 disabled:cursor-default"
                 value={Number(cartItemQuantity)}
               />
-              <span className="sr-only">{Number(cartItemQuantity)} pieces</span>
             </div>
             {/* QUANTITY */}
 
             {/* INCREMENT */}
             <cartItemFetcher.Form method="POST" action="/cart">
-              <button
-                data-action-id="increment-cart"
+              <Button
+                data-action-id="decrement-cart"
                 type="submit"
                 disabled={isMoreThanMax}
-                className={cn(
-                  "text-green-500 hover:text-green-800",
-                  isMoreThanMax && "text-gray-300 hover:text-gray-300"
-                )}>
-                <AiOutlinePlus className="h-6 w-6" />
-              </button>
+                variant={isMoreThanMax ? "disabled" : "outline"}
+                className="border-zinc-400">
+                <AiOutlinePlus className="text-sm" />
+              </Button>
               <input
                 type="hidden"
                 name="_action"
@@ -175,6 +154,21 @@ export function CartItem({
               />
             </cartItemFetcher.Form>
             {/* INCREMENT */}
+
+            {/* DELETE */}
+            <cartItemFetcher.Form method="post" action="/cart" className="mr-8">
+              <Button
+                type="submit"
+                data-action-id="remove-from-cart"
+                className="block">
+                <span className="sr-only">Remove from Cart</span>
+                <MdOutlineDelete className="text-lg text-white" />
+              </Button>
+              <input type="hidden" name="_action" value="delete-item-in-cart" />
+              <input type="hidden" name="cartItemId" value={cartItem?.id} />
+              <input type="hidden" name="orderItemId" value={cartItem?.id} />
+            </cartItemFetcher.Form>
+            {/* DELETE */}
           </div>
         </div>
       </div>
