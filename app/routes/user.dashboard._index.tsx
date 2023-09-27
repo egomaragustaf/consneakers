e71ae216ab6ead83187b7da190951b9b0e7c2f0f
Ego Maragustaf <egomaragustaf@gmail.com>
2023-09-27 22:05:57 +0700

    feat: add user dashboard route

---
+++ app/routes/user.dashboard._index.tsx
@@ -0,0 +1,56 @@
+import type { LoaderArgs, V2_MetaFunction } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+
+import { prisma } from "~/db.server";
+import { authenticator } from "~/services";
+import { useLoaderData } from "@remix-run/react";
+import { Sidebar, Layout } from "~/components";
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "User Dashboard" },
+    { name: "description", content: "User Dahsboard" },
+  ];
+};
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const userSession = await authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+  });
+  if (!userSession?.id) return redirect("/logout");
+
+  const user = await prisma.user.findUnique({
+    where: { id: userSession.id },
+  });
+  if (!user) return redirect("/logout");
+
+  return json({ user });
+};
+
+export default function Route() {
+  const { user } = useLoaderData<typeof loader>();
+
+  if (!user) {
+    return (
+      <Layout>
+        <p>Sorry something went wrong</p>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <main className="flex gap-8 justify-start items-start min-h-screen">
+        <div className="w-60 flex">
+          <Sidebar />
+        </div>
+
+        <div>
+          <header className="space-y-2">
+            <h1 className="text-2xl">Welcome, {user.username}!</h1>
+          </header>
+        </div>
+      </main>
+    </Layout>
+  );
+}
