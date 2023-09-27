a93b3194d701886447f94b9c4564d57dbd2f2a2d
Ego Maragustaf <egomaragustaf@gmail.com>
2023-09-27 17:15:24 +0700

    feat: conditional rendering for admin and user dashboard

--- app/components/shared/sidebar.tsx
+++ app/components/shared/sidebar.tsx
@@ -1,6 +1,7 @@
 import { cn } from "~/utils";
 import { buttonVariants } from "~/components";
 import { NavLink } from "@remix-run/react";
+import { useRootLoaderData } from "~/hooks";
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
@@ -25,7 +26,20 @@ const adminSideBarItems = [
   },
 ];
 
+const userSideBarItems = [
+  {
+    to: "/user/dashboard",
+    text: "Profile",
+  },
+];
+
 export function Sidebar({ className }: SidebarProps) {
+  const { userData } = useRootLoaderData();
+
+  const isAdmin = userData?.username === "admin";
+
+  console.log(isAdmin);
+
   return (
     <div className={cn("pb-4 rounded fixed", className)}>
       <div className="space-y-4 py-4">
@@ -33,26 +47,52 @@ export function Sidebar({ className }: SidebarProps) {
           <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
             Dashboard
           </h2>
-          <div className="space-y-1 flex flex-col">
-            {adminSideBarItems.map((item) => {
-              return (
-                <NavLink
-                  key={item.to}
-                  to={item.to}
-                  className={({ isActive }) =>
-                    cn(
-                      buttonVariants({ variant: "ghost" }),
-                      isActive
-                        ? "bg-zinc-800 hover:bg-zinc-700 text-white hover:text-white"
-                        : "hover:bg-muted",
-                      "justify-start"
-                    )
-                  }>
-                  {item.text}
-                </NavLink>
-              );
-            })}
-          </div>
+
+          {isAdmin && (
+            <div className="space-y-1 flex flex-col">
+              {adminSideBarItems.map((item) => {
+                return (
+                  <NavLink
+                    key={item.to}
+                    to={item.to}
+                    className={({ isActive }) =>
+                      cn(
+                        buttonVariants({ variant: "ghost" }),
+                        isActive
+                          ? "bg-zinc-800 hover:bg-zinc-700 text-white hover:text-white"
+                          : "hover:bg-muted",
+                        "justify-start"
+                      )
+                    }>
+                    {item.text}
+                  </NavLink>
+                );
+              })}
+            </div>
+          )}
+
+          {!isAdmin && (
+            <div className="space-y-1 flex flex-col">
+              {userSideBarItems.map((item) => {
+                return (
+                  <NavLink
+                    key={item.to}
+                    to={item.to}
+                    className={({ isActive }) =>
+                      cn(
+                        buttonVariants({ variant: "ghost" }),
+                        isActive
+                          ? "bg-zinc-800 hover:bg-zinc-700 text-white hover:text-white"
+                          : "hover:bg-muted",
+                        "justify-start"
+                      )
+                    }>
+                    {item.text}
+                  </NavLink>
+                );
+              })}
+            </div>
+          )}
         </div>
       </div>
     </div>
