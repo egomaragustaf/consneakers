6514b984c2fc09d03b67281d64b5a553e883ec3b
Ego Maragustaf <egomaragustaf@gmail.com>
2023-09-30 16:22:22 +0700

    feat: add card cart item on dashboard admin

--- app/routes/admin.dashboard._index.tsx
+++ app/routes/admin.dashboard._index.tsx
@@ -4,7 +4,15 @@ import { json, redirect } from "@remix-run/node";
 import { prisma } from "~/db.server";
 import { authenticator } from "~/services";
 import { useLoaderData } from "@remix-run/react";
-import { Sidebar, Layout } from "~/components";
+import {
+  Sidebar,
+  Layout,
+  Card,
+  CardHeader,
+  CardContent,
+  CardDescription,
+} from "~/components";
+import { useRootLoaderData } from "~/hooks";
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -29,6 +37,7 @@ export const loader = async ({ request }: LoaderArgs) => {
 
 export default function Route() {
   const { user } = useLoaderData<typeof loader>();
+  const { cart } = useRootLoaderData();
   const isAdmin = user?.username === "admin";
 
   if (!user || !isAdmin) {
@@ -48,10 +57,23 @@ export default function Route() {
           <Sidebar />
         </div>
 
-        <div>
+        <div className="flex flex-col gap-4">
           <header className="space-y-2">
             <h1 className="text-2xl">Welcome, {user.username}!</h1>
           </header>
+
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+            <Card className="border border-zinc-300 hover:bg-zinc-50 rounded-xl w-52 p-4">
+              <CardHeader className="flex items-center justify-start">
+                <h1>Cart Items Quantity</h1>
+              </CardHeader>
+              <CardContent>
+                <CardDescription className="text-4xl font-bold text-start">
+                  {cart?.totalQuantity}
+                </CardDescription>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </main>
     </Layout>
